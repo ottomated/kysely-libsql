31b737f7aa5dc60e554f3a2d94143a0525984e26
Ottomated <otto@ottomated.net>
2025-02-06 13:57:14 -0800

    update example

--- example/index.ts
+++ example/index.ts
@@ -1,51 +1,53 @@
 import { Kysely, Generated } from "kysely";
-import { LibsqlDialect } from "@libsql/kysely-libsql";
+import { LibsqlDialect } from "kysely-libsql";
 
 interface Database {
-    book: BookTable,
+  book: BookTable;
 }
 
 interface BookTable {
-    id: Generated<number>,
-    author: string,
-    title: string,
-    year: number | null,
+  id: Generated<number>;
+  author: string;
+  title: string;
+  year: number | null;
 }
 
 async function example() {
-    const db = new Kysely<Database>({
-        dialect: new LibsqlDialect({
-            url: "file:test.db",
-        }),
-    });
+  const db = new Kysely<Database>({
+    dialect: new LibsqlDialect({
+      url: "file:test.db",
+    }),
+  });
 
-    await db.schema
-        .createTable("book")
-        .addColumn("id", "integer", col => col.primaryKey())
-        .addColumn("author", "text", col => col.notNull())
-        .addColumn("title", "text", col => col.notNull())
-        .addColumn("year", "integer")
-        .execute();
+  await db.schema
+    .createTable("book")
+    .addColumn("id", "integer", (col) => col.primaryKey())
+    .addColumn("author", "text", (col) => col.notNull())
+    .addColumn("title", "text", (col) => col.notNull())
+    .addColumn("year", "integer")
+    .execute();
 
-    await db
-        .insertInto("book")
-        .values([
-            { author: "Jane Austen", title: "Sense and Sensibility", year: 1811 },
-            { author: "Daniel Defoe", title: "Robinson Crusoe", year: 1719 },
-            { author: "Sally Rooney", title: "Beautiful World, Where Are You?", year: 2021 },
-        ])
-        .execute();
+  await db
+    .insertInto("book")
+    .values([
+      { author: "Jane Austen", title: "Sense and Sensibility", year: 1811 },
+      { author: "Daniel Defoe", title: "Robinson Crusoe", year: 1719 },
+      {
+        author: "Sally Rooney",
+        title: "Beautiful World, Where Are You?",
+        year: 2021,
+      },
+    ])
+    .execute();
 
-    const books = await db
-        .selectFrom("book")
-        .selectAll()
-        .orderBy("year", "asc")
-        .execute();
-    for (const book of books) {
-        console.dir(book);
-    }
+  const books = await db
+    .selectFrom("book")
+    .selectAll()
+    .orderBy("year", "asc")
+    .execute();
+  console.log(books);
 
-    await db.destroy();
+  await db.destroy();
 }
 
 example();
